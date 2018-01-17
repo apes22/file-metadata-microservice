c0850a38551a77b334a9f646d1bdc74501a680bb
Maribel Duran <maribelduranucm@gmail.com>
2018-01-17 14:02:57 -0800

    Automatically delete uploaded file.

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,20 @@
 const router = require('express').Router();
 const path = require('path');
+const fs = require('fs');
 const bodyParser = require('body-parser');
 const multer  = require('multer');
 const upload = multer({dest: './public/uploads/',
                       limits: {fileSize: 10000000, files:1},
                       });
-
-/*https://developer.mozilla.org/en-US/docs/Learn/HTML/Forms/Sending_forms_through_JavaScript*/
-/* https://www.terlici.com/2015/05/16/uploading-files-locally.html*/
-/* https://pqina.nl/filepond/ */
-/* http://www.dropzonejs.com/#installation */
 router.route('/get-file-size/')
-// req.body will hold the text fields, if there were any 
+// req.body will hold the text fields, if there is any 
 .post( upload.single('file'), function (req, res, next) {
-  // req.file is the `avatar` file 
   let fileInfo = {
     "file_name": req.file.originalname,
     "file_size": `${req.file.size} bytes`
   };
+  //Deleting the file because we currently are not managing the upload files  
+  fs.unlinkSync(req.file.path);
   res.json(fileInfo);
 })
 
